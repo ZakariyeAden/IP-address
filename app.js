0939be11342c242c7f38358bffdb6c9e1d11af3a
Zakariye Aden <zakariye.aden00@gmail.com>
2022-04-16 14:45:47 -0500

    finish styling

---
+++ app.js
@@ -0,0 +1,6 @@
+// API Key from geoipify
+// https://geo.ipify.org/api/v2/country?apiKey=at_4LNjxGuB5mnBywgPqO5iyXc03YmYc&ipAddress=8.8.
+// let map = document.querySelector('#map')
+// var map = L.map('map').setView([51.505, -0.09], 13);
+
+const ApiUrl = ""
